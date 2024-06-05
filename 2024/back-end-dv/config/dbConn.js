c9eedc2c35da1e25b64a5361b9d197e1aedd6941
dushan indunil <81338081+dushancode@users.noreply.github.com>
2024-06-05 17:09:34 +0530

    2

--- 2024/back-end-dv/config/dbConn.js
+++ 2024/back-end-dv/config/dbConn.js
@@ -2,24 +2,24 @@ const mongoose = require('mongoose')
 
 const ur = 'mongodb+srv://dushan:1234@cluster0.hkhm3tj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(ur)
-//     } catch (err) {
-//         console.log(err)
-//     }
-// }
-
-
 const connectDB = async () => {
-    try{
-        await mongoose.connect(ur);
-        console.log("connect to mongodb")
-
-    }
-    catch(er){
-                 console.log(er)
+    try {
+        await mongoose.connect(ur)
+    } catch (err) {
+        console.log(err)
     }
 }
+
+
+// const connectDB = async () => {
+//     try{
+//         await mongoose.connect(ur);
+//         console.log("connect to mongodb")
+
+//     }
+//     catch(er){
+//                  console.log(er)
+//     }
+// }
 
 module.exports = connectDB
